07eec32adb0636b411aa4caef0c587db5d5fdb3b
vejee3 <balabat.vj@gmail.com>
2026-04-14 16:08:20 +0800

    fix error

--- app/Filament/Resources/MemberDetails/Pages/CreateMemberDetail.php
+++ app/Filament/Resources/MemberDetails/Pages/CreateMemberDetail.php
@@ -8,4 +8,13 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateMemberDetail extends CreateRecord
 {
     protected static string $resource = MemberDetailResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (empty($data['branch_id']) && auth()->user()?->branchId()) {
+            $data['branch_id'] = auth()->user()->branchId();
+        }
+
+        return $data;
+    }
 }
