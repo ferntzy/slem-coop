7f9bf7327a30379e2c51faeff590fb31cf13e481
Meru <mhelanthonygone17@gmail.com>
2026-04-27 06:11:44 +0800

    SMTP Credentials

--- scripts/drop_sent_emails.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require __DIR__.'/../bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-DB::statement('DROP TABLE IF EXISTS sent_emails');
-echo "Table dropped\n";
