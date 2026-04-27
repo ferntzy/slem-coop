bf072ea8fcdcb5e6dc0239b8675669acb00ee6d4
Elijah Batestil <elijahbatestil26@gmail.com>
2026-04-27 20:51:45 +0800

    adjust get savings

--- routes/api.php
+++ routes/api.php
@@ -145,7 +145,7 @@ Route::post('/member/fetch-notifications', [Notifications::class, 'fetchNotifica
 Route::post('/member/fetch-unread-notifications', [Notifications::class, 'fetchUnreadNotifications']);
 Route::post('/member/delete-notification', [Notifications::class, 'deleteNotification']);
 Route::post('/member/mark-notification-seen', [Notifications::class, 'markAsRead']);
-Route::post('/member/savings/{id}', [ControllersSavingsAccount::class, 'getSavingsAccount']);
+Route::get('/member/savings/{id}', [ControllersSavingsAccount::class, 'getSavingsAccount']);
 
 // Route::get('/newsevent', [NewsEventController::class, 'show']);
 // Route::get('/newsevent/hero', [HeroNewsEventController::class, 'show']);
