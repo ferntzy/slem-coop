08875dc80093556b3c96c02b77465e299f056679
mrllsndy <rafaeljade343@gmail.com>
2026-05-04 19:44:45 +0800

    instead of edit i change to view

---
+++ app/Filament/Resources/LoanApplications/Pages/ViewLoanApplications.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\LoanApplications\Pages;
+
+use App\Filament\Resources\LoanApplications\LoanApplicationsResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewLoanApplications extends ViewRecord
+{
+    protected static string $resource = LoanApplicationsResource::class;
+
+    public function getTitle(): string
+    {
+        return 'View '.$this->record->member?->profile?->first_name;
+    }
+}
